c169194db4e16a0c3c33bc3123768dfb7d244bbd
Raúl Pedroche <r.pedroche.novillo@gmail.com>
2017-05-13 16:57:26 +0200

    Fix bundle path in entry point script

--- docker/docker-rails-entrypoint.sh
+++ docker/docker-rails-entrypoint.sh
@@ -2,10 +2,10 @@
 case "$1" in
   '')
     # Normal run
-    exec /usr/bin/bundle exec unicorn -p 3000 -c config/unicorn.rb
+    exec /usr/bin/local/bundle exec unicorn -p 3000 -c config/unicorn.rb
     ;;
-  rake|irb)
-    exec /usr/bin/bundle exec "$@"
+  rails|rake|irb)
+    exec /usr/local/bin/bundle exec "$@"
     ;;
   *)
     exec "$@"

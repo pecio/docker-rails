b241903bba291fc7a827d0c88aaa5ac36d408ca2
Raúl Pedroche <r.pedroche.novillo@gmail.com>
2017-05-14 14:15:10 +0200

    Add basic puma configuration

---
+++ app/docker/config/puma.rb
@@ -0,0 +1,20 @@
+# Puma configuration
+# Based on rails 5 default configuration
+port 3000
+environment "production"
+
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+threads threads_count, threads_count
+workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+
+preload_app!
+
+before_fork do
+  ActiveRecord::Base.connection_pool.disconnect! if defined?(ActiveRecord)
+end
+
+on_worker_boot do
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+end
+
+plugin :tmp_restart
